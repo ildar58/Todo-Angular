4d5bd820082ded0ac4ec6451ab93766ac90d47a3
ildar <ildar.dautov01@gmail.com>
2021-06-17 11:09:19 +0300

    feat(filters): добавлена возможность работы как с control

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {priorityLevels} from '../../common/dicts/priority-levels';
 import {statusTypes} from '../../common/dicts/status-types';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {ControlValueAccessor, FormControl, FormGroup} from '@angular/forms';
+import {AppComponentClass} from '../../common/classes/app-component.class';
 
 @Component({
   selector: 'todo-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss']
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent extends AppComponentClass implements ControlValueAccessor, OnInit {
   public priorityLevels = [
     ...priorityLevels,
     {
@@ -18,23 +19,41 @@ export class FiltersComponent implements OnInit {
   ]
   public statusTypes = statusTypes;
 
-  public filterForm;
+  public filterForm = new FormGroup({
+    priority: new FormControl(''),
+    statusTypes: new FormGroup({
+      active: new FormControl(false),
+      failed: new FormControl(false),
+      success: new FormControl(false)
+    }),
+    sortByDate: new FormControl('asc'),
+    sortByPriority: new FormControl('asc'),
+    text: new FormControl('')
+  });
 
-  constructor(private readonly _fb: FormBuilder) {
-    this.filterForm = this._fb.group({
-      priority: '',
-      statusTypes: this._fb.group({
-        low: false,
-        medium: false,
-        high: false
-      }),
-      sortByDate: 'asc',
-      sortByPriority: 'asc',
-      text: ''
-    })
+  public handleChangeSort(event): void {
+    const name = event.active;
+    const value = event.direction;
+    const control = this.filterForm.get(name) as FormControl;
+
+    control.setValue(value);
   }
+
+  writeValue(value: any): void {
+    if (value === Object(value)) {
+      this.filterForm.patchValue(value);
+    }
+  }
+  registerOnChange(fn: any): void {
+    this._propagateChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+
+  private _propagateChange: (value?: any | any[]) => any | any[] = () => {};
 
   ngOnInit(): void {
+    this._observeSafe(this.filterForm.valueChanges).subscribe(value => this._propagateChange(value))
   }
-
 }
